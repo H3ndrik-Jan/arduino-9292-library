562f67ea5b3e570f927df1326305269d0d933d2e
H3ndrik-Jan <46112431+H3ndrik-Jan@users.noreply.github.com>
2019-11-22 20:51:53 +0100

    Add files via upload

--- arduino_9292.cpp
+++ arduino_9292.cpp
@@ -1,38 +1,36 @@
-
 #include "arduino_9292.h"
-
 #include "Arduino.h"
 
-
-//#include "WiFi.h"
 HTTPClient http;
-
 WiFiClient client;
 
-String getTime(void)
+String getTime(int dataType)
 {
-  String payload;
   String timeURL = "http://worldtimeapi.org/api/timezone/Europe/Amsterdam";
-  http.begin(client, timeURL);
+  String payload = _9292GetData(timeURL);
   
-  int httpCode = http.GET();
-  if (httpCode > 0){
-    if (httpCode == HTTP_CODE_OK)
-    {
-      payload = http.getString(); 
-    }
-  }
-  http.end();
   int lookPos = payload.indexOf("\"datetime");
   int untilDate = payload.indexOf("T", lookPos);
-  String timeAndDate = payload.substring((lookPos + 12), (untilDate+6));
-  timeAndDate[13] = '\0'; //remove ':' from the string so the variable can be pasted into the url
+  String timeAndDate;
+  
+  switch(dataType)
+  {
+	case 0:
+	timeAndDate = payload.substring((lookPos + 12), (untilDate+6));
+	timeAndDate.remove(13, 1); //remove ':' from the string so the variable can be pasted into the url
+	break;
+  
+	case 1:
+	timeAndDate = payload.substring((untilDate + 1) , (untilDate + 6));
+	break;
+	
+  }
   return timeAndDate;
 }
 
 int lijstpositie(String payload, int listPosition, String searchWord)
 {
-  int lookPos;
+  int lookPos = 0;
   for(int i=0; i < (listPosition+1); i++)
   {
     lookPos = payload.indexOf(searchWord, lookPos+1); /*, (lookPos + listPosition));*/
@@ -42,8 +40,8 @@ int lijstpositie(String payload, int listPosition, String searchWord)
 
 String _9292GetDepartureInfo(String payload, int dataType, int listPosition)
 {
-  int lookPos;        //position of the individual char in the string it should look for the data
-  int offset;         //offset from the lookPos to the actual data
+  int lookPos = 0;        //position of the individual char in the string it should look for the data
+  int offset = 0;         //offset from the lookPos to the actual data
   String searchWord;  //the word it will look up in the string
 
   switch (dataType)
@@ -84,7 +82,24 @@ String _9292GetData(String url)
     {
       payload = http.getString(); 
     }
+	  else{
+		 // payload = "Error!";
+		  Serial.println("HTTPCode" + httpCode);
+		  Serial.println("Forcing getString");
+		  delay(1000);
+		  payload = http.getString(); 
+		}
   }
   http.end();
   return payload;
+}
+
+
+String makeURL(String fromStation, String toStation)
+{
+    String urlpart1 = "http://api.9292.nl/0.1/journeys?before=0&sequence=1&byFerry=true&bySubway=true&byBus=true&byTram=true&byTrain=true&lang=nl-NL&from=" + fromStation + "&dateTime=";
+    String urlpart2 = "&searchType=departure&interchangeTime=standard&after=0&to=" + toStation;
+    String URL2;
+    URL2 = urlpart1 + getTime(0) + urlpart2;
+    return URL2;
 }
--- arduino_9292.h
+++ arduino_9292.h
@@ -3,20 +3,25 @@
 #ifndef arduino_9292_h
 #define arduino_9292_h
 
-#include "Arduino.h"
 #include "WiFiClient.h"
+#include "arduino.h"
 #include "HTTPClient.h"
+//HTTPClient http;
 
 //fetches the Dutch time from "http://worldtimeapi.org/api/timezone/Europe/Amsterdam", 
-//and parses it into a format that can be directly inserted into a 9292API URL.
-String getTime(void);		
+//and parses it into a format (date+time) that can be directly inserted into a 9292API URL when a 0 is given as dataType (format 'YYYY-MM-DDThhmm').
+//When dataType is 1, the function returns only the time in the format: 'hh:mm'
+String getTime(int dataType);		
 
 
 //parses the unprocessed string from an departure-times request into the wished data. 
 //The dataType is the kind of information you want, for example the departure time or destination.
 //The listposition is the train you want to know the data of. When 1, it will return the first train coming,
 //and when 6, it will return the 6th train coming.
-String _9292GetInfo(String payload, int dataType, int listPosition);
+String _9292GetDepartureInfo(String payload, int dataType, int listPosition);
+
+//returns a URL which you can use for a journey request. The URL includes the current time and the destination-station
+String makeURL(String fromStation, String toStation);
 
 
 //Fetches and returns an unprocessed string with the data fetched from the given url
